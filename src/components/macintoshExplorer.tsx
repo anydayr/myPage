c971c26d744134bb8e7e0b2500d9d3fb5e88ce7b
anydayr <annyrondon491@gmail.com>
2024-02-21 17:45:19 -0400

    style: explore container

--- src/components/macintoshExplorer.tsx
+++ src/components/macintoshExplorer.tsx
@@ -1,17 +1,16 @@
 import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
 import Macintosh from '../../public/Macintosh.jsx'
 
 const MacintoshExplorer = () => {
   return (
-    <Canvas className="explore_canvas">
+    <>
       <OrbitControls />
       <Suspense fallback={null}>
         <Macintosh />
       </Suspense>
       <Environment preset="night" />
-    </Canvas>
+    </>
   )
 }
 
--- src/containers/explore/explore.tsx
+++ src/containers/explore/explore.tsx
@@ -1,11 +1,14 @@
 import './explore.css'
 import iconMac from '../../assets/apple-logo.png'
 import MacintoshExplorer from '../../components/macintoshExplorer'
+import { Canvas } from '@react-three/fiber'
 const Explore = () => {
   return (
     <div className="explore">
       <div className="explore" style={{ display: 'flex' }}>
-        <MacintoshExplorer />
+        <Canvas className="explore_canvas">
+          <MacintoshExplorer />
+        </Canvas>
         <div>
           <h2 className="explore_title">
             <span style={{ color: '#be3455' }}>Hello</span> world
