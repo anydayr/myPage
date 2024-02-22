9c2fcd567d20d30ad27556ef17966696795a163c
anydayr <annyrondon491@gmail.com>
2024-02-22 16:06:19 -0400

    feat: job grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Contact from './containers/contact-me/contact'
 import Explore from './containers/explore/explore'
 import Home from './containers/home/home'
 import Jobs from './containers/jobs/Jobs'
+import Stack from './containers/stack/stack'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <Explore />
       <Jobs />
+      <Stack />
       <Contact />
     </div>
   )
--- src/containers/jobs/Jobs.tsx
+++ src/containers/jobs/Jobs.tsx
@@ -1,7 +1,16 @@
 import './jobs.css'
 
 const Jobs = () => {
-  return <div className="jobs"></div>
+  return (
+    <div className="jobs">
+      <div className="jobs_grid">
+        <div className="proyect_1">1</div>
+        <div className="proyect_2">2</div>
+        <div className="proyect_3">3</div>
+        <div className="proyect_4">4</div>
+      </div>
+    </div>
+  )
 }
 
 export default Jobs
