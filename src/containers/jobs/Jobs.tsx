638464bd705e8f1fa04e67abcd71d25b05d6dcc0
anydayr <annyrondon491@gmail.com>
2024-02-22 11:39:18 -0400

    feat: delete imports

--- src/containers/jobs/Jobs.tsx
+++ src/containers/jobs/Jobs.tsx
@@ -1,8 +1,4 @@
 import './jobs.css'
-import { useState } from 'react'
-import Folder from '../../assets/folder-icon.png'
-import { MoveLeft, MoveRight } from 'lucide-react'
-import Club from '../../assets/club-terrazas-job.webp'
 
 const Jobs = () => {
   return <div className="jobs"></div>
