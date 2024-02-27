37558d55eeea30ae1defabe134af366d04a6a313
anydayr <annyrondon491@gmail.com>
2024-02-27 14:22:07 -0400

    style: bg color section

--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -6,7 +6,10 @@ const Home = () => {
       <div className="home_container">
         <div style={{ position: 'relative' }}>
           <h1 className="home_title">Anny Rondón</h1>
-          <p className="home_rol">Desarrollador Front-end</p>
+          <p className="home_rol">Desarrollador de software</p>
+          <div style={{ display: 'flex', gap: '8px', color: '#00fed0' }}>
+            <div className="active"></div>Disponible para trabajar
+          </div>
         </div>
       </div>
     </div>
