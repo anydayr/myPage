59b3f92bdb0b3b511f72e35dec7045e7db87b97d
anydayr <annyrondon491@gmail.com>
2024-03-02 22:40:14 -0400

    refactor: images format . And add point

--- src/containers/explore/explore.tsx
+++ src/containers/explore/explore.tsx
@@ -2,17 +2,22 @@ import './explore.css'
 import iconMac from '../../assets/apple-logo.png'
 import MacintoshExplorer from '../../components/macintoshExplorer'
 import { Canvas } from '@react-three/fiber'
-
+import Click from '../../assets/Point.svg'
 const Explore = () => {
   return (
     <div className="explore">
       <div className="explore" style={{ display: 'flex' }}>
-        <div className="explore_canvas">
-          <Canvas style={{ background: '#D9D9D9' }}>
+        <img
+          className="point"
+          src={Click}
+          alt=""
+          style={{ zIndex: '2', position: 'absolute', height: '80px' }}
+        />
+        <div className="explore_canvas" style={{ background: '#D9D9D9' }}>
+          <Canvas>
             <MacintoshExplorer />
           </Canvas>
         </div>
-
         <div style={{ padding: '2em' }}>
           <h2 className="explore_title">
             <span className="explore_hello">Hello</span> world
@@ -21,6 +26,7 @@ const Explore = () => {
             Exploremos esta macintosh de 1984
             <img src={iconMac} alt="" style={{ marginLeft: '8px' }} />
           </p>
+          <p style={{ textAlign: 'center' }}>Haz click y gira</p>
         </div>
       </div>
     </div>
--- src/containers/jobs/Jobs.tsx
+++ src/containers/jobs/Jobs.tsx
@@ -1,50 +1,53 @@
 import './jobs.css'
-
+import Title from '../../assets/title.svg'
 const Jobs = () => {
   return (
     <div className="jobs">
-      <div className="jobs_layout">
-        <div className="proyect_1" style={{ position: 'relative' }}>
-          <div className="overlay">
-            <h3 className="overlay_title">El secreto de tu cocina</h3>
-            <a className="ver-mas">Ver más</a>
+      <div style={{ position: 'relative', height: '100%', overflow: 'hidden' }}>
+        <img className="title_jobs" src={Title} alt="" />
+        <div className="jobs_layout">
+          <div className="proyect_1" style={{ position: 'relative' }}>
+            <div className="overlay">
+              <h3 className="overlay_title">El secreto de tu cocina</h3>
+              <a className="ver-mas">Ver más</a>
+            </div>
           </div>
-        </div>
-        <div className="proyect_2" style={{ position: 'relative' }}>
-          <div className="overlay">
-            <h3 className="overlay_title">Soy personalidad</h3>
-            <a href="https://soypersonalidad.com" className="ver-mas">
-              Ver más
-            </a>
+          <div className="proyect_2" style={{ position: 'relative' }}>
+            <div className="overlay">
+              <h3 className="overlay_title">Soy personalidad</h3>
+              <a href="https://soypersonalidad.com" className="ver-mas">
+                Ver más
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="proyect_3" style={{ position: 'relative' }}>
-          <div className="overlay">
-            <h3 className="overlay_title">Club terrazas</h3>
-            <a
-              href="https://play.google.com/store/apps/details?id=com.club.terrazas.app&hl=es_CO&gl=US"
-              className="ver-mas"
-            >
-              Ver más
-            </a>
+          <div className="proyect_3" style={{ position: 'relative' }}>
+            <div className="overlay">
+              <h3 className="overlay_title">Club terrazas</h3>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.club.terrazas.app&hl=es_CO&gl=US"
+                className="ver-mas"
+              >
+                Ver más
+              </a>
+            </div>
           </div>
-        </div>
-        <div
-          className="proyect_4"
-          style={{
-            position: 'relative',
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <h3 className="the_next">Te animas a ser el siguiente?</h3>
-          <div className="overlay">
-            <a
-              className="ver-mas"
-              href="https://www.linkedin.com/in/anny-rond%C3%B3n-18b967283/"
-            >
-              Contáctame
-            </a>
+          <div
+            className="proyect_4"
+            style={{
+              position: 'relative',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <h3 className="the_next">Te animas a ser el siguiente?</h3>
+            <div className="overlay">
+              <a
+                className="ver-mas"
+                href="https://www.linkedin.com/in/anny-rond%C3%B3n-18b967283/"
+              >
+                Contáctame
+              </a>
+            </div>
           </div>
         </div>
       </div>
