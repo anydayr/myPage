d2597f7aa037f53a5e64a19569c48a3dc12fc824
anydayr <annyrondon491@gmail.com>
2024-05-23 22:04:47 -0400

    fix: hh

--- src/containers/home/confetti.tsx
+++ src/containers/home/confetti.tsx
@@ -28,6 +28,8 @@ const Confetti = () => {
       <p>Develop</p>
       <p>9</p>
       <p>9</p>
+      <p>98</p>
+      <p>9000</p>
       {/* 
       <h2 style={{ fontSize: '200px', color: '#7DBCCF', fontWeight: 100, margin: 0 }}>Celebrar</h2> */}
       {/*    <img src={Balloons} alt="" style={{ position: 'absolute', left: '75%' }} /> */}
